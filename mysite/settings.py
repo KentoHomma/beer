949992c1d5118d7526b13645ed4231f0c32c7379
alcohol_hole_ <57031501+KentoHomma@users.noreply.github.com>
2020-05-05 17:08:09 +0900

    allow ec2's ip

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9n8t*n&j+d%sb6c41oi)o0ty-y5m=lk&+$z3=fqa+afy!$ne-('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '52.194.152.36', 'ec2-52-194-152-36.ap-northeast-1.compute.amazonaws.com']
 
 
 # Application definition
